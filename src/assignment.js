894c954465ac68b256175ab0f39797997f00d729
Stella Eneh <47614571+stellanechi@users.noreply.github.com>
2023-08-31 19:25:19 +0000

    task one and two

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,13 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i =1; i <= destination; i++) sum += i;
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+
+ assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -33,23 +35,28 @@ function sumOfNumbersTo(destination) {
  * @returns the object containing count, sum, arrayOfEvenNumbers from 1 to destination
  */
 function countEvenNumbersWithin(destination) {
-    // Write your code here
-    // get the number from 1 to destination
+  
     let sum = 0;
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for (let i = 1; i<= destination; i++){
+        if (i % 2 === 0){
+            count ++;
+            sum += i;
+            arrayOfEvenNumbers.push(i);
+        }
+    }
+
     return {
-        // property value shorthand
-        // when the property name and the value name are the same
-        // you can just write the property name in your object
+        
         count,
         sum,
         arrayOfEvenNumbers
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+ assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
